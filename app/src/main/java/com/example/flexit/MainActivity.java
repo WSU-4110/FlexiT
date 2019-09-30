e07a1a0c5b23ed9855637d63c6d7cf96600af176
Mohammed Hamza <gh3173@wayne.edu>
2019-09-29 20:29:25 -0400

    Revert "Seperated Register into its own activity"
    This reverts commit da28dfb07d1ba5fd6a7e93b9e6b73b66d6a7a66c.

--- app/src/main/java/com/example/flexit/MainActivity.java
+++ app/src/main/java/com/example/flexit/MainActivity.java
@@ -26,6 +26,8 @@ import com.google.firebase.database.DatabaseError;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private EditText password;
+    private EditText email;
     private Button button_register;
     private Button button_login;
     private FirebaseDatabase dataBase;
@@ -37,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = (EditText) findViewById(R.id.signup_email_input);
+        password =(EditText) findViewById(R.id.signup_password_input);
         button_register = (Button)findViewById(R.id.button_register);
         button_login = (Button)findViewById(R.id.button_login);
         mAuth = FirebaseAuth.getInstance();
@@ -52,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (v == button_register ){
-                    startActivity(new Intent(getApplicationContext(),
-                            RegisterActivity.class));
+                    RegisterUser();
                 }
             }
         });
@@ -67,4 +70,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public  void RegisterUser(){
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        if (TextUtils.isEmpty(Email)){
+            Toast.makeText(this, "A field is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(Password)){
+            Toast.makeText(this, "A field is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task){
+                        try{
+                            //check if registration was successful
+                            if(task.isSuccessful()){
+                                //User registration is succcessful
+                                //start profile activity here
+                                Toast.makeText(MainActivity.this, "registration successful",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "Couldn't register, try again",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
+
 }
