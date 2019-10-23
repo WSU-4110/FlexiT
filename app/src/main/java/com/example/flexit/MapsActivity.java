06d91569d552cc0bd8f9886badccb2d330bc361a
Mohammed Hamza <gh3173@wayne.edu>
2019-10-23 00:59:47 -0400

    Edited the MapsActivity.java

--- app/src/main/java/com/example/flexit/MapsActivity.java
+++ app/src/main/java/com/example/flexit/MapsActivity.java
@@ -8,26 +8,43 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    final String API_KEY = "AIzaSyBub4P07WUYOaqKyA4qcLEbiIIUoUuAjMk";
 
     private GoogleMap mMap;
     private Button map;
     private Button home;
+    private Button search;
+    private Button nearbyGyms;
+
+    double latitude;
+    double longitude;
+    private int PROXIMITY_RADIUS = 10000;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+//    LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +52,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         home = (Button) findViewById(R.id.button_home);
         map = (Button) findViewById(R.id.button_map);
+        search = (Button) findViewById(R.id.search_button);
+        nearbyGyms = (Button) findViewById(R.id.button_searchGym);
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +67,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onClick(View view) {
                 finish();
                 startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMapSearch(v);
             }
         });
 
@@ -78,6 +103,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+
+    //For NearbyGyms feature
+    private String getUrl(double latitude, double longitude, String nearbyPlace){
+        StringBuilder getPlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location=" + latitude + "," + longitude);
+        googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
+        googlePlacesUrl.append("&type=" + nearbyPlace);
+        googlePlacesUrl.append("&sensor=true");
+        googlePlacesUrl.append("&key=" + API_KEY);
+        Log.d("getUrl", googlePlacesUrl.toString());
+        return (googlePlacesUrl.toString());
+    }
 
     /**
      * Manipulates the map once available.
