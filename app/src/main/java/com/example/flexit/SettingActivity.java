b6e5f645c2cb3d1d35bb9aef51bd4db3ba1d24d0
Minhajul Abadeen <minhajul.abadeen@aol.com>
2019-12-01 21:37:39 -0500

    adding ability for deleting andupdating acct in settingactivity

--- app/src/main/java/com/example/flexit/SettingActivity.java
+++ app/src/main/java/com/example/flexit/SettingActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 //https://www.youtube.com/watch?v=hoF5A30fbu4
@@ -27,6 +31,7 @@ public class SettingActivity extends AppCompatActivity {
     private FirebaseDatabase myFireBaseDB;
     private DatabaseReference myRef;
 
+    private static final String TAG = "FragmentActivity";
 
     private EditText fName;
     private EditText lName;
@@ -51,14 +56,14 @@ public class SettingActivity extends AppCompatActivity {
 
         // get reference to 'users' node
         myRef = myFireBaseDB.getReference("users");
-        save = (Button) findViewById(R.id.editbutton1);
+        save = (Button) findViewById(R.id.updateinfo);
         fName = (EditText) findViewById(R.id.text_fName);
         lName = (EditText) findViewById(R.id.text_lName);
         user_age = (EditText) findViewById(R.id.text_age);
         user_weight = (EditText) findViewById(R.id.text_weight);
         user_height = (EditText) findViewById(R.id.text_height);
         user_description = (EditText) findViewById(R.id.text_description);
-        deleteFbUser = (Button)findViewById(R.id.editbutton3);
+        deleteFbUser = (Button)findViewById(R.id.setting_del);
 
         next = (Button) findViewById(R.id.button_next);
 
@@ -77,13 +82,22 @@ public class SettingActivity extends AppCompatActivity {
                 final int weight = Integer.parseInt(user_weight.getText().toString());
                 final String description = user_description.getText().toString();
 
+
                 User user = new User(userID,first,last,age,height,weight,description);
+
+                if (TextUtils.isEmpty(userId)) {
+                    getUserInfo(userID,first,last,age,height,weight,description);
+                }
+                else {
+                    UpdateInfo(userID,first,last,age,height,weight,description);
+                }
 
 
 
             }
         });
 
+        getSettingStatus();
 
         deleteFbUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,6 +119,81 @@ public class SettingActivity extends AppCompatActivity {
                         });
             }
         });
+
+
+
+
+
+
+}
+
+    // Changing button text
+    private void getSettingStatus() {
+        if (TextUtils.isEmpty(userId)) {
+            save.setText("Save");
+        } else {
+            save.setText("Update");
+        }
+    }
+
+
+
+    private void getUserInfo(String uID, String fName, String lName, int age, int height, int weight, String description ) {
+        // TODO
+
+        if (TextUtils.isEmpty(userId)) {
+            userId = myRef.push().getKey();
+        }
+
+        User user = new User(uID,fName,lName,age,height,weight,description);
+
+
+        myRef.child(userId).setValue(user);
+
+        userDataDriver();
+    }
+
+
+    private void userDataDriver() {
+        myRef.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                if (user == null) {
+                    Log.e(TAG, "No Data Found!");
+                    return;
+                }
+
+                Log.e(TAG, "Updated" + user.getUserID() + user.getfName() + user.getAge() + user.getHeight() + user.getWeight());
+
+
+                getSettingStatus();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read user", error.toException());
+            }
+        });
+    }
+    private void UpdateInfo(String uID, String fName, String lName, int age, int height, int weight, String description ) {
+        // updating the user via child nodes
+        if (!TextUtils.isEmpty(fName))
+            myRef.child(userId).child("First Name").setValue(fName);
+            myRef.child(userId).child("Age").setValue(age);
+
+
+        if (!TextUtils.isEmpty(lName))
+            myRef.child(userId).child("Last Name").setValue(lName);
+            myRef.child(userId).child("Height").setValue(height);
+
+
+        if (!TextUtils.isEmpty(description))
+            myRef.child(userId).child("Description").setValue(description);
+            myRef.child(userId).child("Weight").setValue(weight);
+
 
 
     }
