c15248a1402c62771d265a2cc191b36d487b1026
Ryan Kelsey <ryankelsey14@outlook.com>
2022-10-31 20:02:45 -0400

    Implementation of workout filtering and stopwatch improvements
    As a part of group 1's ENGG4450 assignment 2, the group has made two changes to this project. First being improved stopwatch functionalities, and secondly added the ability to filter the workouts by weighted and unweighted.

--- app/src/main/java/com/example/flexit/WorkoutFeed.java
+++ app/src/main/java/com/example/flexit/WorkoutFeed.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.animation.ArgbEvaluator;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.like.LikeButton;
@@ -28,7 +32,8 @@ public class WorkoutFeed extends AppCompatActivity {
     List<Model> models;
     Integer[] colors = null;
     ArgbEvaluator argbEvaluator = new ArgbEvaluator();
-
+    Button button_weighted;
+    int isWeighted = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +45,6 @@ public class WorkoutFeed extends AppCompatActivity {
        // back = (Button)findViewById(R.id.button_back);
 
         models = new ArrayList<>();
-        models.add(new Model(R.drawable.weighted1, "Dumbell Bench Press", "3 Sets 10 Reps Rest for 60sec: " +
-                "Lie on a bench with your feet on the floor directly underneath your knees, " +
-                "holding the dumbbells above your chest. Lower them to your chest, then drive your feet hard into the floor and push the dumbbells back strongly to the start position."));
-        models.add(new Model(R.drawable.weighted2, "Back Squat", "Sets 5 Time 5 Rest 90sec" +
-                "Rest the bar on your back with your feet roughly shoulder-width apart, toes pointing out slightly. " +
-                "Keep your spine in alignment by looking at a spot on the floor about two metres in front of you, then sit back and down as if you were aiming for a chair. " +
-                "Lower until your hip crease is below your knee. As you drive back up, keep your weight on your heels."));
         models.add(new Model(R.drawable.unweighted1, "Wall Sit", "30 secs per set." +
                 "Slowly slide your back down a wall until your thighs are parallel to the floor. " +
                 "Make sure knees are directly above ankles and keep back straight."));
@@ -54,7 +52,13 @@ public class WorkoutFeed extends AppCompatActivity {
                 "Stand with feet together and lunge forward with right foot. " +
                 "Jump straight up, propelling arms forward while keeping elbows bent." +
                 "While in the air, switch legs and land in a lunge with the opposite leg forward. Repeat and continue switching legs."));
-
+        models.add(new Model(R.drawable.weighted1, "Dumbell Bench Press", "3 Sets 10 Reps Rest for 60sec: " +
+                "Lie on a bench with your feet on the floor directly underneath your knees, " +
+                "holding the dumbbells above your chest. Lower them to your chest, then drive your feet hard into the floor and push the dumbbells back strongly to the start position."));
+        models.add(new Model(R.drawable.weighted2, "Back Squat", "Sets 5 Time 5 Rest 90sec" +
+                "Rest the bar on your back with your feet roughly shoulder-width apart, toes pointing out slightly. " +
+                "Keep your spine in alignment by looking at a spot on the floor about two metres in front of you, then sit back and down as if you were aiming for a chair. " +
+                "Lower until your hip crease is below your knee. As you drive back up, keep your weight on your heels."));
 
         adapter = new ModelAdapter(models, this);
 
@@ -70,6 +74,71 @@ public class WorkoutFeed extends AppCompatActivity {
 
         colors = colors_temp;
 
+        button_weighted = findViewById(R.id.button_all);
+        button_weighted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                models.clear();
+                models.add(new Model(R.drawable.unweighted1, "Wall Sit", "30 secs per set." +
+                        "Slowly slide your back down a wall until your thighs are parallel to the floor. " +
+                        "Make sure knees are directly above ankles and keep back straight."));
+                models.add(new Model(R.drawable.unweighted2, "Lunge Jump", " 10 reps Rest for 60 sec: " +
+                        "Stand with feet together and lunge forward with right foot. " +
+                        "Jump straight up, propelling arms forward while keeping elbows bent." +
+                        "While in the air, switch legs and land in a lunge with the opposite leg forward. Repeat and continue switching legs."));
+                models.add(new Model(R.drawable.weighted1, "Dumbell Bench Press", "3 Sets 10 Reps Rest for 60sec: " +
+                        "Lie on a bench with your feet on the floor directly underneath your knees, " +
+                        "holding the dumbbells above your chest. Lower them to your chest, then drive your feet hard into the floor and push the dumbbells back strongly to the start position."));
+                models.add(new Model(R.drawable.weighted2, "Back Squat", "Sets 5 Time 5 Rest 90sec" +
+                        "Rest the bar on your back with your feet roughly shoulder-width apart, toes pointing out slightly. " +
+                        "Keep your spine in alignment by looking at a spot on the floor about two metres in front of you, then sit back and down as if you were aiming for a chair. " +
+                        "Lower until your hip crease is below your knee. As you drive back up, keep your weight on your heels."));
+
+                adapter = new ModelAdapter(models, getApplicationContext());
+
+                viewPager.setAdapter(adapter);
+
+            }
+        });
+        button_weighted = findViewById(R.id.button_weighted);
+        button_weighted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                models.clear();
+                models.add(new Model(R.drawable.weighted1, "Dumbell Bench Press", "3 Sets 10 Reps Rest for 60sec: " +
+                        "Lie on a bench with your feet on the floor directly underneath your knees, " +
+                        "holding the dumbbells above your chest. Lower them to your chest, then drive your feet hard into the floor and push the dumbbells back strongly to the start position."));
+                models.add(new Model(R.drawable.weighted2, "Back Squat", "Sets 5 Time 5 Rest 90sec" +
+                        "Rest the bar on your back with your feet roughly shoulder-width apart, toes pointing out slightly. " +
+                        "Keep your spine in alignment by looking at a spot on the floor about two metres in front of you, then sit back and down as if you were aiming for a chair. " +
+                        "Lower until your hip crease is below your knee. As you drive back up, keep your weight on your heels."));
+
+                adapter = new ModelAdapter(models, getApplicationContext());
+
+                viewPager.setAdapter(adapter);
+
+            }
+        });
+
+        button_weighted = findViewById(R.id.button_body);
+        button_weighted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                models.clear();
+                models.add(new Model(R.drawable.unweighted1, "Wall Sit", "30 secs per set." +
+                        "Slowly slide your back down a wall until your thighs are parallel to the floor. " +
+                        "Make sure knees are directly above ankles and keep back straight."));
+                models.add(new Model(R.drawable.unweighted2, "Lunge Jump", " 10 reps Rest for 60 sec: " +
+                        "Stand with feet together and lunge forward with right foot. " +
+                        "Jump straight up, propelling arms forward while keeping elbows bent." +
+                        "While in the air, switch legs and land in a lunge with the opposite leg forward. Repeat and continue switching legs."));
+
+                adapter = new ModelAdapter(models, getApplicationContext());
+
+                viewPager.setAdapter(adapter);
+
+            }
+        });
 
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
